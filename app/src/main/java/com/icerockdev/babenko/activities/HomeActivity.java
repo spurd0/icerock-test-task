f33dbc001ad4b2433700f36dec00c78aef54266f
Roman Spurdo <babe-roman@yandex.ru>
2017-04-30 17:03:38 +0700

    Added checker for URL.

--- app/src/main/java/com/icerockdev/babenko/activities/HomeActivity.java
+++ app/src/main/java/com/icerockdev/babenko/activities/HomeActivity.java
@@ -2,9 +2,8 @@ package com.icerockdev.babenko.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -12,6 +11,7 @@ import com.icerockdev.babenko.R;
 import com.icerockdev.babenko.data.ApplicationConstants;
 
 public class HomeActivity extends AppCompatActivity {
+    private EditText mRequestUrlEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +21,10 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-        final EditText requestUrlEditText = (EditText) findViewById(R.id.fieldsRequestUrlEditText);
+        mRequestUrlEditText = (EditText) findViewById(R.id.fieldsRequestUrlEditText);
         Button requestFieldsButton = (Button) findViewById(R.id.getFieldsButton);
-        requestUrlEditText.setText(ApplicationConstants.URL_START);
-        requestUrlEditText.setSelection(requestUrlEditText.getText().length());
+        mRequestUrlEditText.setText(ApplicationConstants.URL_START);
+        mRequestUrlEditText.setSelection(mRequestUrlEditText.getText().length());
         requestFieldsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,6 +34,10 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void requestDataFields() {
-
+        String url = mRequestUrlEditText.getText().toString();
+        if (!URLUtil.isValidUrl(url)) {
+            mRequestUrlEditText.setError(getString(R.string.url_error));
+            return;
+        }
     }
 }
