50fbef5b6605fa9c048ee378d6042a926786f85a
Roman Spurdo <babe-roman@yandex.ru>
2017-05-06 21:59:39 +0700

    Moved to ViewGroup for fields` edit text.

--- app/src/main/java/com/icerockdev/babenko/activities/DataFieldsActivity.java
+++ app/src/main/java/com/icerockdev/babenko/activities/DataFieldsActivity.java
@@ -8,20 +8,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.icerockdev.babenko.BuildConfig;
 import com.icerockdev.babenko.R;
-import com.icerockdev.babenko.adapters.DataFieldsAdapter;
 import com.icerockdev.babenko.model.DataField;
 import com.icerockdev.babenko.utils.UtilsHelper;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by Roman Babenko on 01/05/17.
@@ -32,8 +28,7 @@ public class DataFieldsActivity extends AppCompatActivity {
     public static final String DATA_FIELDS_KEY = "com.icerockdev.babenko.activities.DataFieldsActivity.DATA_FIELDS_KEY";
 
     private ArrayList<DataField> mDataFieldsList;
-    private DataFieldsAdapter mFieldsAdapter;
-    private ListView mFieldsLv; // TODO: 06/05/17 MOVE TO ViewGroup 
+    private LinearLayout mDataFieldsContainer;
     private TextView mHeaderErrorTv;
 
     @Override
@@ -45,75 +40,30 @@ public class DataFieldsActivity extends AppCompatActivity {
     }
 
     protected void initViews() {
-        initListView();
-    }
-
-    private void initListView() throws NullPointerException {
-        mFieldsLv = (ListView) findViewById(R.id.dataFieldsListView);
-        if (mDataFieldsList == null)
-            throw new NullPointerException("FieldsListIsNull");
-        mFieldsAdapter = new DataFieldsAdapter(this, R.layout.data_field_element, mDataFieldsList);
-        mFieldsLv.setAdapter(mFieldsAdapter);
-        addFooterToListView();
-        addHeaderToListView();
-    }
-
-    private void addFooterToListView() {
-        LinearLayout footer = new LinearLayout(this);
-        Button submitButton = new Button(this);
-        submitButton.setText(getString(R.string.submit_fields_button_text));
-        LinearLayout.LayoutParams buttonLayoutParams = new LinearLayout
-                .LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-        buttonLayoutParams.setMargins(UtilsHelper.convertDpToPx(this, getResources()
-                .getDimension(R.dimen.data_field_element_footer_margin_horizontal)),
-                UtilsHelper.convertDpToPx(this, getResources()
-                        .getDimension(R.dimen.data_field_element_footer_margin_vertical)),
-                UtilsHelper.convertDpToPx(this, getResources()
-                        .getDimension(R.dimen.data_field_element_footer_margin_horizontal)),
-                UtilsHelper.convertDpToPx(this, getResources()
-                        .getDimension(R.dimen.data_field_element_footer_margin_vertical)));
-        submitButton.setLayoutParams(buttonLayoutParams);
+        mDataFieldsContainer = (LinearLayout) findViewById(R.id.dataFieldsEditTextContainer);
+        initDataFieldsEt();
+        mHeaderErrorTv = (TextView) findViewById(R.id.validationErrorTv);
+        Button submitButton = (Button) findViewById(R.id.submitFieldsButton);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 submitButtonPressed();
             }
         });
-        footer.addView(submitButton);
-        mFieldsLv.addFooterView(footer);
+        showError("Test");
     }
 
-    private void addHeaderToListView() {
-        RelativeLayout headerLayout = new RelativeLayout(this);
-        mHeaderErrorTv = new TextView(this);
-        mHeaderErrorTv.setText(getString(R.string.data_field_incorrect_format));
-        RelativeLayout.LayoutParams errorTvLayoutParams = new RelativeLayout
-                .LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        errorTvLayoutParams.setMargins(UtilsHelper.convertDpToPx(this, getResources()
-                        .getDimension(R.dimen.data_field_element_header_margin_horizontal)),
-                UtilsHelper.convertDpToPx(this, getResources()
-                        .getDimension(R.dimen.data_field_element_header_margin_vertical)),
-                UtilsHelper.convertDpToPx(this, getResources()
-                        .getDimension(R.dimen.data_field_element_header_margin_horizontal)),
-                UtilsHelper.convertDpToPx(this, getResources()
-                        .getDimension(R.dimen.data_field_element_header_margin_vertical)));
-        errorTvLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, 0);
-        errorTvLayoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-        mHeaderErrorTv.setLayoutParams(errorTvLayoutParams);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            mHeaderErrorTv.setTextColor(getColor(R.color.errorTextColor));
-        } else {
-            mHeaderErrorTv.setTextColor(getResources().getColor(R.color.errorTextColor));
+    private void initDataFieldsEt() throws NullPointerException {
+        if (mDataFieldsList == null)
+            throw new NullPointerException("FieldsListIsNull");
+        for (DataField field : mDataFieldsList) {
+            EditText fieldEt = new EditText(this);
+            fieldEt.setText(field.getDefault_value());
+            mDataFieldsContainer.addView(fieldEt);
         }
-        headerLayout.addView(mHeaderErrorTv);
-        mHeaderErrorTv.setVisibility(View.GONE);
-        mFieldsLv.addHeaderView(headerLayout);
     }
 
     private void submitButtonPressed() {
-        HashMap<String, String> fieldsValues = mFieldsAdapter.getFieldValues();
-        for (Map.Entry<String, String> value : fieldsValues.entrySet())
-            Log.d(TAG, value.getValue());
     }
 
     private void getIntentData() {
@@ -124,5 +74,10 @@ public class DataFieldsActivity extends AppCompatActivity {
         }
         if (BuildConfig.DEBUG)
             Log.d(TAG, "DataFields length is " + mDataFieldsList.size());
+    }
+
+    private void showError(String error) {
+        mHeaderErrorTv.setVisibility(View.VISIBLE);
+        mHeaderErrorTv.setText("Test");
     }
 }
