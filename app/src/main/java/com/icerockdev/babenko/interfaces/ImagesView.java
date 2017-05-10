3494cd0dfbdcb8116dff70bcb9f2a5aafb882d4c
Roman Spurdo <babe-roman@yandex.ru>
2017-05-10 20:20:10 +0700

    Added presenter & view for images activity

---
+++ app/src/main/java/com/icerockdev/babenko/interfaces/ImagesView.java
@@ -0,0 +1,9 @@
+package com.icerockdev.babenko.interfaces;
+
+/**
+ * Created by Roman Babenko on 10/05/17.
+ */
+
+public interface ImagesView extends BaseView {
+
+}
---
+++ app/src/main/java/com/icerockdev/babenko/presenters/ImagesPresenter.java
@@ -0,0 +1,10 @@
+package com.icerockdev.babenko.presenters;
+
+import com.icerockdev.babenko.interfaces.ImagesView;
+
+/**
+ * Created by Roman Babenko on 10/05/17.
+ */
+
+public class ImagesPresenter extends BasePresenter<ImagesView> {
+}
