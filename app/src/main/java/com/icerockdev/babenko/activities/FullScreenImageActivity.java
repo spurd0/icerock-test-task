9332c365c6b929247aa5c422a1f1c5252fc0008f
Roman Babenko <r.babenko@amber.software>
2017-05-11 15:44:57 +0700

    Added layout for new activity.

--- app/src/main/java/com/icerockdev/babenko/activities/FullScreenImageActivity.java
+++ app/src/main/java/com/icerockdev/babenko/activities/FullScreenImageActivity.java
@@ -1,11 +1,27 @@
 package com.icerockdev.babenko.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.icerockdev.babenko.R;
 
 /**
  * Created by Roman Babenko on 5/11/2017.
  */
 
 public class FullScreenImageActivity extends AppCompatActivity {
-    // TODO: 5/11/2017 make fullscreen
+    private ImageView mImageView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: 5/11/2017 make fullscreen
+        setContentView(R.layout.activity_full_screen_image);
+    }
+
+    private void initViews() {
+        mImageView = (ImageView) findViewById(R.id.fullScreenImageView);
+    }
 }
