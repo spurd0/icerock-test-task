e4a6fcd48a1bd85bf87b3ccd15cb0acda9d505e3
Roman Babenko <r.babenko@amber.software>
2017-05-11 17:04:25 +0700

    Added pinch-to-zoom option.

--- app/src/main/java/com/icerockdev/babenko/activities/FullScreenImageActivity.java
+++ app/src/main/java/com/icerockdev/babenko/activities/FullScreenImageActivity.java
@@ -1,14 +1,17 @@
 package com.icerockdev.babenko.activities;
 
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.icerockdev.babenko.BuildConfig;
 import com.icerockdev.babenko.R;
-import com.icerockdev.babenko.interfaces.BaseView;
 import com.squareup.picasso.OkHttpDownloader;
 import com.squareup.picasso.Picasso;
 
@@ -16,10 +19,20 @@ import com.squareup.picasso.Picasso;
  * Created by Roman Babenko on 5/11/2017.
  */
 
-public class FullScreenImageActivity extends AppCompatActivity implements BaseView {
+public class FullScreenImageActivity extends AppCompatActivity implements View.OnTouchListener {
     public static final String IMAGE_URL_KEY = "com.icerockdev.babenko.presenters.FullScreenImagePresenter.IMAGE_URL_KEY";
 
     private ImageView mImageView;
+
+    Matrix matrix = new Matrix();
+    Matrix savedMatrix = new Matrix();
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    int mode = NONE;
+    PointF start = new PointF();
+    PointF mid = new PointF();
+    float oldDist = 1f;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,5 +58,61 @@ public class FullScreenImageActivity extends AppCompatActivity implements BaseVi
             }
         }).downloader(new OkHttpDownloader(this)).build();
         picasso.load(imageUrl).into(mImageView);
+        mImageView.setOnTouchListener(this);
     }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {  // just code from stackoverflow
+        ImageView view = (ImageView) v;
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                savedMatrix.set(matrix);
+                start.set(event.getX(), event.getY());
+                mode = DRAG;
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                oldDist = spacing(event);
+                if (oldDist > 10f) {
+                    savedMatrix.set(matrix);
+                    midPoint(mid, event);
+                    mode = ZOOM;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+                mode = NONE;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (mode == DRAG) {
+                    matrix.set(savedMatrix);
+                    matrix.postTranslate(event.getX() - start.x, event.getY()
+                            - start.y);
+                } else if (mode == ZOOM) {
+                    float newDist = spacing(event);
+                    if (newDist > 10f) {
+                        matrix.set(savedMatrix);
+                        float scale = newDist / oldDist;
+                        matrix.postScale(scale, scale, mid.x, mid.y);
+                    }
+                }
+                break;
+        }
+
+        view.setImageMatrix(matrix);
+        return true;
+    }
+
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (float) Math.sqrt(x * x + y * y);
+    }
+
+    private void midPoint(PointF point, MotionEvent event) {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
+    }
+
 }
