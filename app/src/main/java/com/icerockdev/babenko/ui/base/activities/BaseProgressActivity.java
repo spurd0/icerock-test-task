855fe091bbe2add2f3b73cbe99654e4a74accaca
Roman Babenko <rbab@yandex.ru>
2018-04-29 17:51:27 +0700

    Different fixes.

--- app/src/main/java/com/icerockdev/babenko/ui/base/activities/BaseProgressActivity.java
+++ app/src/main/java/com/icerockdev/babenko/ui/base/activities/BaseProgressActivity.java
@@ -1,5 +1,7 @@
 package com.icerockdev.babenko.ui.base.activities;
 
+import android.os.Bundle;
+
 import com.icerockdev.babenko.ui.base.fragments.ProgressDialogFragment;
 import com.icerockdev.babenko.ui.base.views.ProgressBaseView;
 
@@ -9,9 +11,13 @@ import com.icerockdev.babenko.ui.base.views.ProgressBaseView;
 
 public abstract class BaseProgressActivity extends BaseActivity implements ProgressBaseView {
     protected String mDialogTag = "BaseProgressActivity.PROGRESS_DIALOG_TAG";
+    private boolean allowToShowDialog;
 
     @Override
     public void showProgressDialog() {
+        if (!allowToShowDialog) {
+            return;
+        }
         ProgressDialogFragment progressDialogFragment = (ProgressDialogFragment) getSupportFragmentManager().
                 findFragmentByTag(mDialogTag);
         if (progressDialogFragment != null) {
@@ -24,11 +30,26 @@ public abstract class BaseProgressActivity extends BaseActivity implements Progr
 
     @Override
     public void dismissProgressDialog() {
+        if (!allowToShowDialog) {
+            return;
+        }
         ProgressDialogFragment progressDialogFragment = (ProgressDialogFragment) getSupportFragmentManager().
                 findFragmentByTag(mDialogTag);
         if (progressDialogFragment != null) {
             progressDialogFragment.dismiss();
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        allowToShowDialog = false;
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        allowToShowDialog = true;
     }
 
     @Override
--- app/src/main/java/com/icerockdev/babenko/ui/splash/SplashView.java
+++ app/src/main/java/com/icerockdev/babenko/ui/splash/SplashView.java
@@ -1,15 +1,12 @@
 package com.icerockdev.babenko.ui.splash;
 
-import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
 import com.arellomobile.mvp.viewstate.strategy.SkipStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 import com.icerockdev.babenko.ui.base.views.BaseView;
 
-@StateStrategyType(AddToEndSingleStrategy.class)
+@StateStrategyType(SkipStrategy.class)
 public interface SplashView extends BaseView {
-    @StateStrategyType(SkipStrategy.class)
     void startImagesActivity();
 
-    @StateStrategyType(SkipStrategy.class)
     void startLoginActivity();
 }
